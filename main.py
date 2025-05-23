e6f21cc2f659d39e4594c841196564114142461d
zayro-official <sushirepublic.surfside@gmail.com>
2025-05-22 22:21:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
+import os
+from dotenv import load_dotenv
+from supabase import create_client, Client
+
+# 環境変数の読み込み
+load_dotenv()
+
+# Supabaseクライアントの初期化
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_API_KEY")
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
@@ -10,7 +22,9 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("index.html", {
+        "request": request
+    })
 
 @app.get("/reviews", response_class=HTMLResponse)
 async def read_reviews(request: Request):
